3717b1fd4f0a8190853c4925e701a02cace530ae
mariwerty <kedamv1.22@gmail.com>
2023-12-17 09:25:21 +0300

    final task

--- src/test/java/com/stv/bdd/steps/MyFinalTaskSteps.java
+++ src/test/java/com/stv/bdd/steps/MyFinalTaskSteps.java
@@ -30,7 +30,6 @@ public class MyFinalTaskSteps extends BasicFactoryTest {
         driver.get(START_URL);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        sleep.doSleep(1000);
     }
 
 
@@ -39,38 +38,33 @@ public class MyFinalTaskSteps extends BasicFactoryTest {
         if (!Driver.getDriver().findElements(By.xpath("//button[contains(text(),'Accept all')]")).isEmpty()){
             mainFactoryPage.clickOnTrustButton();
         } else {
-            sleep.doSleep(1000);
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         }
     }
 
     @When("The basket icon is clicked")
     public void theBasketIconIsClicked() {
         mainFactoryPage.clickOnBasketIcon();
-        sleep.doSleep(1000);
     }
 
     @Then("The empty basket page is displayed")
     public void theEmptyBasketPageIsDisplayed() {
         mainFactoryPage.isEmptyBasketTitleDisplayed();
-        sleep.doSleep(1000);
     }
 
     @When("The Sign In button is pressed")
     public void theSignInButtonIsPressed() {
         mainFactoryPage.clickOnSignInButton();
-        sleep.doSleep(1000);
     }
 
     @Then("The login page is opened")
     public void theLoginPageIsOpened() {
         loginPage.isLoginContainerDisplayed();
-        sleep.doSleep(1000);
     }
 
     @When("The continue button is pressed")
     public void theContinueButtonIsPressed() {
         loginPage.clickContinueButton();
-        sleep.doSleep(1000);
     }
 
     @When("The user clicks the Sign In icon")
@@ -82,40 +76,33 @@ public class MyFinalTaskSteps extends BasicFactoryTest {
     @And("The forgot password link in clicked")
     public void theForgotPasswordLinkInClicked() {
         loginPage.clickForgotPassword();
-        sleep.doSleep(1000);
     }
 
     @And("The password recovery page is opened")
     public void thePasswordRecoveryPageIsOpened() {
         loginPage.isForgottenPasswordContainerDisplayed();
-        sleep.doSleep(1000);
     }
     @And("The user inputs {string}")
     public void theUserInputs(String email) {
         loginPage.email.sendKeys(email);
         Assert.assertEquals(loginPage.email.getAttribute("value"), email);
-        sleep.doSleep(1000);
     }
 
 
     @And("The user clicks Continue Button")
     public void theUserClicksContinueButton() {
         loginPage.clickContinueButton();
-        sleep.doSleep(1000);
     }
 
     @Then("Validation error appears")
     public void validationErrorAppears() {
         loginPage.isWrongEmailMessageDisplayed();
-        sleep.doSleep(1000);
     }
 
     @And("The user clears the input")
     public void theUserClearsTheInput() {
         loginPage.email.clear();
         Assert.assertEquals(loginPage.email.getAttribute("value"), "");
-        sleep.doSleep(1000);
     }
-
 
 }
